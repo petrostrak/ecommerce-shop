81332720debe655175e2523ae2b9d83ee3574e4b
petros <pit.trak@gmail.com>
2020-07-23 13:05:43 +0300

    added firebase properties at environment.ts/ environmet.prod.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDH6BQJSLjWM1YZaYb9R-b-8lMG5OQXqbo",
+    authDomain: "ecommerce-shop-91fe3.firebaseapp.com",
+    databaseURL: "https://ecommerce-shop-91fe3.firebaseio.com",
+    projectId: "ecommerce-shop-91fe3",
+    storageBucket: "ecommerce-shop-91fe3.appspot.com",
+    messagingSenderId: "905997156612",
+    appId: "1:905997156612:web:e11d920037e8e7d9fb9051",
+    measurementId: "G-BDNJ0QGWX5"
+  }
 };
 
 /*
