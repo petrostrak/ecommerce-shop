888d894f721c9520f0917faaca78929144725a2f
root <root@petros-thinkpad.petros-thinkpad.net>
2020-07-23 21:42:55 +0300

    minor changes @ navbar.css and auth.service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -15,7 +15,7 @@ export class AuthService {
   }
 
   login(){
-    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl');
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     localStorage.setItem('returnUrl', returnUrl);
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
