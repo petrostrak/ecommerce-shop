48a50e3f8f12793f4eeedb8d5edede51b79ce215
root <root@petros-thinkpad.petros-thinkpad.net>
2020-07-24 14:41:04 +0300

    editing a product

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,8 @@
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators'
 
 @Component({
   selector: 'app-product-form',
@@ -10,9 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
+  product = {};
 
-  constructor(private router:Router, private categoryService: CategoryService, private productService: ProductService) {
+  constructor(private route: ActivatedRoute, private router:Router, private categoryService: CategoryService, private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
+    let id = this.route.snapshot.paramMap.get('id')
+    if(id) this.productService.get(id).valueChanges().pipe(take(1)).subscribe(p => this.product = p)
   }
 
   save(product){
